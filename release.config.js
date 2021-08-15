ea9769bd94373b60ba11dda1ed6b714700028534
makotot <makoto.ttn@gmail.com>
2021-08-15 20:03:04 +0900

    fix: automatically update version field

--- release.config.js
+++ release.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   branches: ['main'],
   repositoryUrl: 'git@github.com:makotot/GhostUI.git',
+  plugins: [
+    '@semantic-release/git',
+    {
+      message:
+        // eslint-disable-next-line no-template-curly-in-string
+        'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+  ],
 };
